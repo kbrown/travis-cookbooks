41e4dde6ed6b8a25bd1bd86577f5a761c1bd0cde
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-05-28 14:30:50 +0200

    Create customized /etc/hosts

--- ci_environment/euc2014/attributes/default.rb
+++ ci_environment/euc2014/attributes/default.rb
@@ -5,3 +5,12 @@ default[:euc2014] = {}
 default[:euc2014][:user] = user
 default[:euc2014][:group] = node.euc2014.user
 default[:euc2014][:home] = "/home/#{node.euc2014.user}"
+default[:euc2014][:lazy_hostname] = lambda { `hostname` }
+default[:euc2014][:hosts] = [{:name => "mim-1",
+                              :ip => "172.28.128.11"},
+                             {:name => "mim-2",
+                              :ip => "172.28.128.12"},
+                             {:name => "tsung-1",
+                               :ip => "172.28.128.21"},
+                             {:name => "tsung-2",
+                              :ip => "172.28.128.22"}]
--- ci_environment/euc2014/recipes/default.rb
+++ ci_environment/euc2014/recipes/default.rb
@@ -1,1 +1,8 @@
-# intentionally empty (you know, git doesn't like empty files)
+template "/etc/hosts" do
+  source "etc.hosts.erb"
+  mode "0644"
+  owner "root"
+  group "root"
+  variables :hostname => node.euc2014.lazy_hostname.call,
+            :hosts => node.euc2014.hosts
+end
