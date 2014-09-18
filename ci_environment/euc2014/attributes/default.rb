12427783558e30220b288e98be8444bc08cbed95
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-09-18 16:01:24 +0200

    Run only MongooseIM instances

--- ci_environment/euc2014/attributes/default.rb
+++ ci_environment/euc2014/attributes/default.rb
@@ -13,9 +13,6 @@ default[:euc2014][:hosts] = [{:name => "mim-1",
                              {:name => "mim-2",
                               :ip => "172.28.128.12",
                               :roles => [:mongooseim]},
-                             {:name => "tsung-1",
-                              :ip => "172.28.128.21",
-                              :roles => [:tsung]},
-                             {:name => "tsung-2",
-                              :ip => "172.28.128.22",
-                              :roles => [:tsung]}]
+                             {:name => "mim-3",
+                              :ip => "172.28.128.13",
+                              :roles => [:mongooseim]}]
